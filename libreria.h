e8c4d584e00e7739f951a7d1d47769f39e54f91d
pedromind <pedro.as18@gmail.com>
2015-09-08 09:23:01 -0300

    Se definen librerías

---
+++ libreria.h
@@ -0,0 +1,19 @@
+#ifndef STDLIB_H
+#include <stdlib.h>
+#definde STDLIB_H
+#endif
+
+#ifndef STDIO_H
+#include <stdio.h>
+#definde STDIO_H
+#endif
+
+typdef struc tnodo{
+	char dato;
+	struct tnodo *anterior;
+	struct tnodo *siguiente;
+}Nodo;
+
+main(){
+}
+
